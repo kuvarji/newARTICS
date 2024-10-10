a16f0df9be11374922be913f056723aad3071228
Kuvarji Gupta <139498248+kuvarji@users.noreply.github.com>
2024-10-10 13:00:21 +0000

    nnl

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,9 @@
 const userModel = require("../models/user-model.js")
 const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const cookie = require("cookie-parser")
+
+
 const {generateToken} = require("../utils/generateToken.js")
-const flash = require("connect-flash")
+
 
 
 module.exports.registeruser = 
